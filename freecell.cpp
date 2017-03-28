320dc92c120ffbd00f5104659b2b14b96ccf3ad7
locmark <kkraczyna@gmail.com>
2017-03-28 17:41:12 +0200

    controls - beginning

--- freecell.cpp
+++ freecell.cpp
@@ -80,14 +80,79 @@ char getValueChar (unsigned short int value) {
   }
 }
 
+
+char mark (unsigned short int x, unsigned short int y, unsigned short int pos_x, unsigned short int pos_y, unsigned short int pos_x_marked, unsigned short int pos_y_marked, bool marked) {
+  if (x == pos_x && y == pos_y) {
+    if (marked) {
+      if (pos_x == pos_x_marked && pos_y == pos_y_marked) {
+        return static_cast<char>(178);
+      } else {
+        return static_cast<char>(176);
+      }
+    } else {
+      return static_cast<char>(176);
+    }
+  } else {
+    if (marked && x == pos_x_marked && y == pos_y_marked) {
+      return static_cast<char>(177);
+    } else {
+      return ' ';
+    }
+  }
+}
+
 class game_t {
 public:
   vector<card_t> board[8];
   card_t target[4];
   card_t aux[4];    // auxiliary
-  void draw () {
+  void draw (unsigned short int x, unsigned short int y, unsigned short int x_marked, unsigned short int y_marked, bool marked) {
+    for (size_t i = 0; i < 8; i++) {
+      cout << static_cast<char>(201);
+      for (size_t k = 0; k < 9; k++) {
+        cout << static_cast<char>(205);
+      }
+      cout << static_cast<char>(187);
+    }
+    cout << endl;
+
+    for (size_t j = 0; j < 8; j++) {
+      cout << static_cast<char>(186);
+      for (size_t k = 0; k < 3; k++) {
+        cout << static_cast<char>(mark(j, 0, x, y, x_marked, y_marked, marked));
+      }
+      cout << "XXX";
+      for (size_t k = 0; k < 3; k++) {
+        cout << static_cast<char>(mark(j, 0, x, y, x_marked, y_marked, marked));
+      }
+      cout << static_cast<char>(186);
+    }
+    cout << endl;
+
+    for (size_t i = 0; i < 4; i++) {
+      for (size_t j = 0; j < 8; j++) {
+        cout << static_cast<char>(186);
+        for (size_t k = 0; k < 9; k++) {
+          cout << static_cast<char>(mark(j, 0, x, y, x_marked, y_marked, marked));
+        }
+        cout << static_cast<char>(186);
+      }
+      cout << endl;
+    }
+
+    for (size_t i = 0; i < 8; i++) {
+        cout << static_cast<char>(200);
+        for (size_t k = 0; k < 9; k++) {
+          cout << static_cast<char>(205);
+        }
+        cout << static_cast<char>(188);
+      }
+    cout << endl;
 
     bool endDrawing = false;
+
+    size_t i = 0;
+
     for (size_t i = 0; !endDrawing; i++) {
       endDrawing = true;
 
@@ -100,9 +165,25 @@ public:
           }
           cout << static_cast<char>(187);
         } else {
-          cout << "           ";
+          if (i - 2 <= board[j].size()) {
+            endDrawing = false;
+            if (i - 2 < board[j].size()) {
+              cout << static_cast<char>(186);
+              for (size_t k = 0; k < 9; k++) {
+                cout << static_cast<char>(mark(j, i, x, y, x_marked, y_marked, marked));
+              }
+              cout << static_cast<char>(186);
+            }else{
+              cout << static_cast<char>(200);
+              for (size_t k = 0; k < 9; k++) {
+                cout << static_cast<char>(205);
+              }
+              cout << static_cast<char>(188);
+            }
+          }else{
+            cout << "           ";
+          }
         }
-
       }
 
       cout << endl;
@@ -110,15 +191,26 @@ public:
       for (size_t j = 0; j < 8; j++) {
         if (board[j].size() > i) {
           endDrawing = false;
-          cout << static_cast<char>(186) << "   ";
+          cout << static_cast<char>(186);
+          for (size_t k = 0; k < 3; k++) {
+            cout << static_cast<char>(mark(j, i + 1, x, y, x_marked, y_marked, marked));
+          }
           if (board[j][i].getValue() == 10)
             cout << setw(2) << 10;
           else
             cout << setw(2) << getValueChar(board[j][i].getValue());
           drawSymbol(board[j][i].getType());
-          cout << "   " << static_cast<char>(186);
+          for (size_t k = 0; k < 3; k++) {
+            cout << static_cast<char>(mark(j, i + 1, x, y, x_marked, y_marked, marked));
+          }
+          cout << static_cast<char>(186);
         } else {
-          cout << "           ";
+          if (i - 2 < board[j].size()) {
+            endDrawing = false;
+            cout << static_cast<char>(186) << "         " << static_cast<char>(186);
+          }else{
+            cout << "           ";
+          }
         }
       }
       cout << endl;
@@ -128,6 +220,12 @@ public:
 
 
 int main(int argc, char const *argv[]) {
+  HWND console = GetConsoleWindow();
+  RECT r;
+  GetWindowRect(console, &r); //stores the console's current dimensions
+
+  //MoveWindow(window_handle, x, y, width, height, redraw_window);
+  MoveWindow(console, r.left, r.top, 800, 600, TRUE);
   game_t game;
 
 
@@ -152,8 +250,44 @@ int main(int argc, char const *argv[]) {
   exampleCard.setValue(10);
   game.board[2].push_back(exampleCard);
   // *****************
-  game.draw();
+  // game.draw();
 
+
+  unsigned short int x = 0;
+  unsigned short int y = 0;
+  unsigned short int x_marked = 0;
+  unsigned short int y_marked = 0;
+  bool marked = false;
+  char c;
+  game.draw(x, y, x_marked, y_marked, marked);
+  while (1) {
+    c = getch();
+    switch (c) {
+      case 'w':
+        y--;
+        break;
+      case 's':
+        y++;
+        break;
+      case 'a':
+        x--;
+        break;
+      case 'd':
+        x++;
+        break;
+      case ' ':
+      if (marked && x == x_marked && y == y_marked) {
+        marked = false;
+      } else {
+        marked = true;
+        x_marked = x;
+        y_marked = y;
+      }
+        break;
+    }
+    system("cls");
+    game.draw(x, y, x_marked, y_marked, marked);
+  }
 
 
 
